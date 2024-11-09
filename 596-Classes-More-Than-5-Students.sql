85a4c6a9557c2c521d959c6fa2e7f0cb17e103b5
Reem Al-Mahdi <112731236+Reemaa828@users.noreply.github.com>
2024-11-09 19:17:38 +0200

    [596] [Time Beats: 97.71%] - LeetPush

--- 596-Classes-More-Than-5-Students.sql
+++ 596-Classes-More-Than-5-Students.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
-with cte_try as 
-(select class,count(student) from courses group by class having count(Student) >= 5)
 
-select class from cte_Try ;
+select class from courses group by class having count(Student) >= 5
+
+
 
