3f6a6d4594f1dd3b2ca4c7624d3944f71a41a906
Reem Al-Mahdi <112731236+Reemaa828@users.noreply.github.com>
2024-11-11 22:25:15 +0200

    [1070] [Time Beats: 85.81%] - LeetPush

--- 1070-Product-Sales-Analysis-III.sql
+++ 1070-Product-Sales-Analysis-III.sql
@@ -1,7 +1,9 @@
 # Write your MySQL query statement below
-with cte as(
-select 
-    product_id,year as first_year,quantity,price,rank()over(partition by product_id order by year ) l from 
-    sales inner join product using(product_id) 
-)
-select product_id,first_year,quantity,price from cte where cte.l =1
+-- with cte as(
+-- select 
+--     product_id,year as first_year,quantity,price,rank()over(partition by product_id order by year ) l from 
+--     sales 
+-- )
+-- select product_id,first_year,quantity,price from cte where cte.l =1
+select product_id,year as first_year,quantity,price from sales
+where(product_id,year) in (select product_id, min(year)  from sales inner join product using(product_id) group by product_id );
