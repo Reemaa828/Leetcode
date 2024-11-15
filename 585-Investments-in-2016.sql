8e1d34b7c93a01378248f257b08c2e006c1460f2
Reem Al-Mahdi <112731236+Reemaa828@users.noreply.github.com>
2024-11-15 06:17:10 +0200

    [585] [Time Beats: 18.79%] - LeetPush

---
+++ 585-Investments-in-2016.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select round(sum(tiv_2016),2) as tiv_2016 from insurance where tiv_2015 =any (select tiv_2015 from insurance group by tiv_2015 having count(tiv_2015) >=2 ) and (lat,lon)=any(select lat,lon from insurance group by lat,lon having count(*)=1) ;
